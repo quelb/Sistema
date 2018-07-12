303bdf73b3717532521b56730d0cd230e3fe1264
oficialds <danielelouzeiro@gmail.com>
2018-07-12 09:15:12 -0300

    Update Endereco.java

--- sistema/src/main/java/com/mycompany/sistema/Endereco.java
+++ sistema/src/main/java/com/mycompany/sistema/Endereco.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.sistema;
+package com.mycompany.;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
