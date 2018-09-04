f0d97bbc53b3b2b68304d026e32cd1c9800feb8a
quelbarcelos <quequel.barcelos6@gmail.com>
2018-09-04 09:42:06 -0300

    página de erro funcional

--- sitephp/loginServlet.php
+++ sitephp/loginServlet.php
@@ -1,43 +1,31 @@
 <?php
-//LOGIN
-//ACHO QUE é ISSO...
+
 $error=''; 
 if(isset($_POST['submit'])){
  if(empty($_POST['email']) || empty($_POST['senha'])){
- $error = "errologin.php";
+ $error = "Usuário ou senha errado";
  }
  else
  {
- //Defina uma variavel e muda o que esta em ["nome no bd"];
- $user=$_POST['email'];
- $pass=$_POST['senha'];
- //Deixa o nome do host normal...Root tambem...Se você estiver na escola....Coloca "root" nos "" sem nada...
- $conn = mysqli_connect("localhost", "root", "root");
- //Entre os "coloca o nome do BD"
- $db = mysqli_select_db($conn, "infinitecloset");
+ $usuario = $_POST['email'];
+ $senha = $_POST['senha'];
+ $conn = mysqli_connect("127.0.0.1", "root", "root");
+ $db = mysqli_select_db($conn, "MaBiBookz");
  
- 
- //coloque entre FROM e Where o nome da tabela do bd e depois de WHERE coloca o que você colocou nas string...
- //MEU BRAÇO
- //GUARAPARI
- //MINHA ARTE
- //YOU FRONT
- //Só segue.... É só copiar....
- $query = mysqli_query($conn, "SELECT * FROM usuario WHERE senha='$pass' AND email='$user'");
+ $query = mysqli_query($conn, "SELECT * FROM usuario WHERE senha='$senha' AND email='$usuario'");
  
  
  $rows = mysqli_num_rows($query);
  if($rows == 1){
- header("Location: meuperfil.php"); // coloca a pagina que voces quiserem(recomendo a de login, pra ficar mais fácil)
+ header("Location: perfil.php"); // coloca a pagina que voces quiserem(recomendo a de login, pra ficar mais fácil)
  }
  else
  {
  $error = "Usuario ou senha errado";
+ header("Location: errologin.php");
  }
  mysqli_close($conn); // ISSO AQUI É UM tipo de return...Parando o codigo....(return não para)
  }
 }
- //By yo e google...
- //3 info
- //cultura
+
 ?>
